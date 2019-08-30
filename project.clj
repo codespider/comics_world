1ef19f64058e8f7fb22d359cba96dbcc87a53547
karthik <karthikkannan@gmail.com>
2019-08-30 21:16:55 +0530

    Added mount states for starting & stopping server.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [mount "0.1.16"]]
   :main ^:skip-aot comics-world.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/comics_world/core.clj
+++ src/comics_world/core.clj
@@ -1,16 +1,23 @@
 (ns comics-world.core
-  (:require [ring.adapter.jetty :refer [run-jetty]])
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [mount.core :refer [defstate]])
   (:gen-class))
 
 
 
+
 (defn handler [request]
-  {:status 200
+  {:status  200
    :headers {"Content-Type" "text/html"}
-   :body "Hello World!!!"})
+   :body    "Hello World!!! 1234567"})
 
 (defn start-server [port]
-  (run-jetty handler {:port port}))
+  (run-jetty handler {:port port :join? false}))
+
+(defstate app-server
+  :start (start-server 3000)
+  :stop (.stop app-server))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
